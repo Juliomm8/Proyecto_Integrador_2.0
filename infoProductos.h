3a77cbb095588dfda225c09d71fdff37eb899ab0
Juliomm <150650620+Juliomm8@users.noreply.github.com>
2024-12-21 20:34:29 -0500

    Update infoProductos.h
    Se realizaron los siguientes cambios:
    - Reemplazo de múltiples arreglos (nombres, códigos, etc.) por un arreglo único de estructuras `struct Producto`.
    - Actualización de las funciones `ingresarProducto` e `imprimirTodosLosProductos` para trabajar con la estructura.
    - Simplificación del acceso a los datos mediante el uso de atributos como `inventario[i].nombre`.
    - Mejora de la legibilidad y modularidad del código al consolidar los datos de los productos en una estructura centralizada.
    
    Este cambio mejora la eficiencia y escalabilidad del sistema al manejar los datos del inventario.

--- infoProductos.h
+++ infoProductos.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #include "validación.h"
+#include "estructuraProducto.h"
 
-void ingresarProducto(char nombres[][50], char codigos[][20], char fabricantes[][50], int *cantidades, float *precios, float *impuestos, int *requierePrescripcion, int *numProductos) {
+void ingresarProducto(struct Producto inventario[], int *numProductos) {
     // Verificar si el inventario está lleno
     if (*numProductos >= 1000) {
         printf("\033[1;31mNo se pueden agregar más productos [Inventario lleno]\033[0m\n");
@@ -18,12 +19,12 @@ void ingresarProducto(char nombres[][50], char codigos[][20], char fabricantes[]
     int nombreValido = 0;
     do {
         printf("Ingresa el nombre del producto: ");
-        fgets(nombres[*numProductos], sizeof(nombres[*numProductos]), stdin);
-        nombres[*numProductos][strcspn(nombres[*numProductos], "\n")] = '\0';
+        fgets(inventario[*numProductos].nombre, sizeof(inventario[*numProductos].nombre), stdin);
+        inventario[*numProductos].nombre[strcspn(inventario[*numProductos].nombre, "\n")] = '\0';
 
         // Validar si el nombre es único
-        if (!esNombreUnico(nombres[*numProductos], nombres, *numProductos)) {
-            printf("\033[1;31mEl nombre del producto ya existe, ingresa con otro nombre.\033[0m\n");
+        if (!esNombreUnico(inventario[*numProductos].nombre, inventario, *numProductos)) {
+            printf("\033[1;31mEl nombre del producto ya existe, ingresa otro nombre.\033[0m\n");
         } else {
             nombreValido = 1;
         }
@@ -31,15 +32,15 @@ void ingresarProducto(char nombres[][50], char codigos[][20], char fabricantes[]
 
     // Ingreso del fabricante
     printf("Ingresa el nombre del fabricante: ");
-    fgets(fabricantes[*numProductos], sizeof(fabricantes[*numProductos]), stdin);
-    fabricantes[*numProductos][strcspn(fabricantes[*numProductos], "\n")] = '\0';
+    fgets(inventario[*numProductos].fabricante, sizeof(inventario[*numProductos].fabricante), stdin);
+    inventario[*numProductos].fabricante[strcspn(inventario[*numProductos].fabricante, "\n")] = '\0';
 
     // Ingreso del código del producto
     int codigoValido = 0;
     do {
-        ingresarCodigoValido(codigos[*numProductos], sizeof(codigos[*numProductos]));
-        if (!esCodigoUnico(codigos[*numProductos], codigos, *numProductos)) {
-            printf("\033[1;31mEl código ya está en uso, ingresa otro\033[0m\n");
+        ingresarCodigoValido(inventario[*numProductos].codigo, sizeof(inventario[*numProductos].codigo), inventario, *numProductos);
+        if (!esCodigoUnico(inventario[*numProductos].codigo, inventario, *numProductos)) {
+            printf("\033[1;31mEl código ya está en uso, ingresa otro.\033[0m\n");
         } else {
             codigoValido = 1;
         }
@@ -48,50 +49,50 @@ void ingresarProducto(char nombres[][50], char codigos[][20], char fabricantes[]
      // Ingreso de la cantidad de unidades
     do {
         printf("Cantidad de unidades: ");
-        scanf("%d", &cantidades[*numProductos]);
-        getchar();//limpiar el buffer
-        if (!validarCantidadProducto(cantidades[*numProductos])) {
-            printf("\033[1;31mCantidad no válida, ingresa de nuevo\033[0m\n");
+        scanf("%d", &inventario[*numProductos].cantidad);
+        getchar(); // Limpiar el buffer
+        if (!validarCantidadProducto(inventario[*numProductos].cantidad)) {
+            printf("\033[1;31mCantidad no válida, ingresa de nuevo.\033[0m\n");
         }
-    } while (!validarCantidadProducto(cantidades[*numProductos]));
+    } while (!validarCantidadProducto(inventario[*numProductos].cantidad));
 
     // Ingreso del precio sin impuestos
     do {
         printf("Precio del producto (sin impuestos): ");
-        scanf("%f", &precios[*numProductos]);
+        scanf("%f", &inventario[*numProductos].precio);
         getchar(); // Limpiar el buffer
-        if (precios[*numProductos] <= 0) {
-            printf("\033[1;31mEl precio debe ser mayor que 0, ingresa de nuevo\033[0m\n");
+        if (inventario[*numProductos].precio <= 0) {
+            printf("\033[1;31mEl precio debe ser mayor que 0, ingresa de nuevo.\033[0m\n");
         }
-    } while (precios[*numProductos] <= 0);
+    } while (inventario[*numProductos].precio <= 0);
 
     // Ingreso del porcentaje de impuestos
     do {
         printf("Porcentaje del impuesto para este producto: ");
-        scanf("%f", &impuestos[*numProductos]);
+        scanf("%f", &inventario[*numProductos].impuesto);
         getchar(); // Limpiar el buffer
-        if (impuestos[*numProductos] < 0) {
-            printf("\033[1;31mEl impuesto no puede ser negativo, ingresa de nuevo\033[0m\n");
+        if (inventario[*numProductos].impuesto < 0) {
+            printf("\033[1;31mEl impuesto no puede ser negativo, ingresa de nuevo.\033[0m\n");
         }
-    } while (impuestos[*numProductos] < 0);
-
+    } while (inventario[*numProductos].impuesto < 0);
+    
     // Ingreso de si requiere prescripción médica
     do {
         printf("\033[1;31m-----¿Requiere prescripción médica?-----\033[0m\n");
         printf("\033[1;31mSí [1]\033[0m  \033[1;32mNo [0]\033[0m : ");
-        scanf("%d", &requierePrescripcion[*numProductos]);
+        scanf("%d", &inventario[*numProductos].requierePrescripcion);
         getchar(); // Limpiar el buffer
-        if (requierePrescripcion[*numProductos] != 0 && requierePrescripcion[*numProductos] != 1) {
-            printf("\033[1;31mOpción no válida, ingresa 1 para Sí o 0 para No\033[0m\n");
+        if (inventario[*numProductos].requierePrescripcion != 0 && inventario[*numProductos].requierePrescripcion != 1) {
+            printf("\033[1;31mOpción no válida, ingresa 1 para Sí o 0 para No.\033[0m\n");
         }
-    } while (requierePrescripcion[*numProductos] != 0 && requierePrescripcion[*numProductos] != 1);
+    } while (inventario[*numProductos].requierePrescripcion != 0 && inventario[*numProductos].requierePrescripcion != 1);
 
     // Incrementar el contador de productos
-    (*numProductos)++;
+  (*numProductos)++;
     printf("\033[1;32m-----Producto agregado con éxito :)-----\033[0m\n");
 }
 
-void imprimirTodosLosProductos(char nombres[][50], char codigos[][20], char fabricantes[][50], int *cantidades, float *precios, float *impuestos, int *requierePrescripcion, int numProductos) {
+void imprimirTodosLosProductos(struct Producto inventario[], int numProductos) {
     // Validar si el inventario está vacío
     if (numProductos == 0) {
         printf("\033[1;31m-----No hay productos en el inventario-----\033[0m\n");
@@ -101,18 +102,19 @@ void imprimirTodosLosProductos(char nombres[][50], char codigos[][20], char fabr
      // Imprimir información de cada producto
     printf("\033[1;34m\nLista de productos:\033[0m\n");
     for (int i = 0; i < numProductos; i++) {
-        float precioConImpuestos = precios[i] * (1 + impuestos[i] / 100);
+        float precioConImpuestos = inventario[i].precio * (1 + inventario[i].impuesto / 100);
         printf("\033[1;33mProducto %d:\033[0m\n", i + 1);
-        printf("  Nombre: %s\n", nombres[i]);
-        printf("  Código: %s\n", codigos[i]);
-        printf("  Fabricante: %s\n", fabricantes[i]);
-        printf("  Unidades: %d\n", cantidades[i]);
-        printf("  Precio (sin impuestos): %.2f\n", precios[i]);
-        printf("  Impuesto: %.2f%%\n", impuestos[i]);
+        printf("  Nombre: %s\n", inventario[i].nombre);
+        printf("  Código: %s\n", inventario[i].codigo);
+        printf("  Fabricante: %s\n", inventario[i].fabricante);
+        printf("  Unidades: %d\n", inventario[i].cantidad);
+        printf("  Precio (sin impuestos): %.2f\n", inventario[i].precio);
+        printf("  Impuesto: %.2f%%\n", inventario[i].impuesto);
         printf("  P.V.P: %.2f\n", precioConImpuestos);
-        printf("  Requiere prescripción: %s\n", requierePrescripcion[i] ? "\033[1;31mSí\033[0m" : "\033[1;32mNo\033[0m");
-        printf("\033[1;34m----------------------------------------\033[0m\n");
+        printf("  Requiere prescripción: %s\n", inventario[i].requierePrescripcion ? "\033[1;31mSí\033[0m" : "\033[1;32mNo\033[0m");
+            printf("\033[1;34m----------------------------------------\033[0m\n");
+        }
     }
-}
+
 
 #endif // INFOPRODUCTOS_H
